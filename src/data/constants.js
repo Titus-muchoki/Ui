3bb17e5bada047d66d2a36f5e3ae83da06cd8550
Titus-muchoki <93070153+Titus-muchoki@users.noreply.github.com>
2023-08-27 21:45:30 +0300

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,9 +7,9 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://drive.google.com/file/d/1LiGFnXUT0qjHZkYt7Yml4TW-cGCERQsu/view?usp=sharing",
+  github: "https://github.com/Titus-muchoki",
   resume:
-    "",
+    "https://drive.google.com/file/d/1LiGFnXUT0qjHZkYt7Yml4TW-cGCERQsu/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/titus-muchoki-4261a0245/",
   twitter: "https://twitter.com/KajelaTo?s=09",
   insta: "https://www.instagram.com/kajela_to_the_world/",
